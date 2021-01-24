cdfb4c326a44b13d397636fa86d4989f28a2ca4a
LuisCampi <lcampi@espol.edu.ec>
2021-01-23 19:00:22 -0500

    Actualizando

---
+++ components/header.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function Header() {
+    return (
+      <View style={styles.header}>
+        <Text style={styles.title}>SELECCIONE LOS TIPOS DE LAVADO QUE DESEE PARA SU VEHICULO</Text>
+      </View>
+    );
+  }
+  
+  const styles = StyleSheet.create({
+    header: {
+      height: 120,
+      paddingTop: 38,
+      backgroundColor: 'blue',
+    },
+    title: {
+      textAlign: 'center',
+      color: '#fff',
+      fontSize: 20,
+      fontWeight: 'bold',
+    }
+  });
