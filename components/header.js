60b11cf85b39927da749340129bc14b2b2c2b0f2
LuisCampi <lcampi@espol.edu.ec>
2021-01-23 19:09:17 -0500

    Actualizando

--- components/header.js
+++ components/header.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function Header() {
     return (
       <View style={styles.header}>
-        <Text style={styles.title}>SELECCIONE LOS TIPOS DE LAVADO QUE DESEE PARA SU VEHICULO</Text>
+        <Text style={styles.title}>SELECCIONE LOS TIPOS DE SERVICIOS QUE DESEE PARA SU VEHICULO</Text>
       </View>
     );
   }
