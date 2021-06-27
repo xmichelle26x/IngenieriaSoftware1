57c75837ebe8e9ba446beddc77aa258ab1ad8b02
xmichelle26x <xmichelle26x@gmail.com>
2021-06-27 14:13:35 -0500

    cambios

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,7 @@ require('dotenv').config({ path: 'variables.env' })
 
 const conectarDB = async () => {
   try {
-    await mongoose.connect(process.env.DB_MONGO, {
+    await mongoose.connect(DB_MONGO, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useFindAndModify: false,
